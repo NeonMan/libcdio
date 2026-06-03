0bf6b32525ce2063c87564f46243195b973563ee
Shiva Kiran Koninty <shiva@skran.xyz>
2026-06-03 14:49:48 +0530

    Use thread locals in recursion check to allow for multi threaded use
    libcdio's log methods have a recursion check that uses a global
    static. While originally intended to prevent recursion, it also
    introduced a side effect that caused multi threaded programs
    to abort due to the check causing an assert call.
    
    Use a thread local static to keep the recursion check and
    prevent program abort during multi threaded use.

--- lib/driver/portable.h
+++ lib/driver/portable.h
@@ -51,4 +51,12 @@
 # define drand48()   (rand() / (double)RAND_MAX)
 #endif
 
+#if defined(_MSC_VER)
+# define THREAD_LOCAL static __declspec(thread)
+#elif defined(__GNUC__) || defined(__clang__)
+# define THREAD_LOCAL static __thread
+#else
+# define THREAD_LOCAL
+#endif
+
 #endif /* CDIO_DRIVER_PORTABLE_H_ */
